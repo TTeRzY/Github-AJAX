2e6e206f6d1f0f30b5b2ecc7ef0b8fecf1bac923
TTeRzY <marin.terziyski@gmail.com>
2017-12-14 16:56:25 +0200

    Commit AJAX request to repo with JSONparse...

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
-function getProfile(){
-    var username = 'TTeRzY';
+
+
+function getProfile(e){
+    e.preventDefault();
+
+    var username = document.getElementById('username').value;
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
+
+
         if (xhttp.readyState == 4 && xhttp.status == 200){
             var user = JSON.parse(xhttp.responseText);
-            document.getElementById('profile').innerHTML = user.name;
+            document.getElementById('profile').innerHTML =
+                `<div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h3 class="panel-title">${user.name}</h3>
+                  </div>
+                  <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <img src="${user.avatar_url}">
+                        </div>
+                        <br><br>
+                        <div class="col-md-9">
+                            <span class="label label-primary">Public Repos ${user.public_repos}</span>
+                            <span class="label label-danger">Public Gists ${user.public_gists}</span>
+                            <ul class="list-group">
+                                <li class="list-group-item">Website: ${user.blog}</li>
+                                <li class="list-group-item">Followers: ${user.followers}</li>
+                                 <li class="list-group-item">Following: ${user.following}</li>
+                            </ul>
+                            <a class="btn btn-default" target="_blank" href="${user.html_url}">Visit Github</a>
+                        </div>
+                    </div>
+                  </div>
+                </div>`;
         }
 
-        console.log(user);
     }
 
-    xhttp.open('GET', 'https://api.github.com/users/' + username, true);
+    xhttp.open('GET', 'https://api.github.com/users/'+username, true);
     xhttp.send();
 }
 
-    getProfile();
+   document.getElementById('userForm').addEventListener('submit', getProfile, false);
 
